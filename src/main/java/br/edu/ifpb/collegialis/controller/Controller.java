a01e2166365d56d3b61ed0599cfebf2eef048408
victor <verribeiro@hotmail.com>
2018-11-06 19:08:56 -0200

    Refatorando para padrão front controller + command

---
+++ src/main/java/br/edu/ifpb/collegialis/controller/Controller.java
@@ -0,0 +1,61 @@
+package br.edu.ifpb.collegialis.controller;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import commands.Command;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/controller")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    protected void processRequest(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
+        //Executa o Comando conforme a URL
+        Command comando = null;        
+	    try {
+	        comando = (Command)Class.forName("commands."+request.getParameter("command")).newInstance();
+	    } catch (InstantiationException e) {
+	        e.printStackTrace();
+	    } catch (IllegalAccessException e) {
+	        e.printStackTrace();
+	    } catch (ClassNotFoundException e) {
+	        e.printStackTrace();
+	    }
+	  
+       comando.execute(request, response);
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request,response);
+	}
+
+}
